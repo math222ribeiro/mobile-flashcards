ab0295b06c0f3a232964fcb2dbc26095f76ef580
Matheus Ribeiro D'Azevedo Lopes <math222ribeiro@hotmail.com>
2017-12-24 15:49:04 -0200

    New Deck view UI created

--- app/components/NewDeckView.js
+++ app/components/NewDeckView.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
-import {View, Text} from 'react-native';
-import {Ionicons} from '@expo/vector-icons'
+import {KeyboardAvoidingView, StyleSheet, View} from 'react-native';
+import {Ionicons} from '@expo/vector-icons';
+import {FormInput, FormLabel, Button} from 'react-native-elements';
 
 class NewDeckView extends Component {
   static navigationOptions = {
@@ -8,13 +9,42 @@ class NewDeckView extends Component {
     tabBarIcon: ({ tintColor, focused }) => <Ionicons name={focused ? 'ios-add-circle' : 'ios-add-circle-outline'} size={30} color={tintColor} />
   };
 
+  state = {
+    input: ''
+  };
+
   render() {
     return (
-      <View>
-        <Text>NEW DECK VIEW </Text>
-      </View>
+      <KeyboardAvoidingView style={{justifyContent: 'space-between', flex:1, flexDirection: 'column'}}>
+        <View>
+          <FormLabel
+            labelStyle={styles.label}
+          >
+            What is the title of your new deck?
+          </FormLabel>
+          <FormInput
+            value={this.state.input}
+            onChangeText={(input) => this.setState({input: input})}
+            placeholder="Deck name"
+            inputStyle={styles.input}
+          />
+        </View>
+        <Button title="Submit" buttonStyle={styles.button}/>
+      </KeyboardAvoidingView>
     )
   }
 }
 
+const styles = StyleSheet.create({
+  label: {
+    fontSize: 30,
+    marginBottom: 30
+  },
+  button: {
+    marginTop: 40,
+  },
+  input: {
+    fontSize: 22
+  }
+});
 export default NewDeckView;
